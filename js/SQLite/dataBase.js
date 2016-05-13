1696ffa9b456c9bd4d656953500fa9eef15ed8e4
arxzel <fabianmillan@unisangil.edu.co>
2016-05-13 16:46:03 -0500

    areglo jquery

--- js/SQLite/dataBase.js
+++ js/SQLite/dataBase.js
@@ -6,7 +6,7 @@ function conexion(){
 		db=window.openDatabase("NombreDeLaDataBase","VersionDeLaDataBase", "PasswordDataBase","TamañoDataBase");
 	*/
 
-    //db=window.openDatabase("miDataBase","1.0","123456",5000);
+    db=window.openDatabase("miDataBase","1.0","123456",5000);
 }
 
 
@@ -35,11 +35,11 @@ function populateDB(){
 		db.transaction(function(tx){contenido...;}, function(err){contenido...;}, function(){contenido...;});
 	*/
 	 	
-    //db.transaction(createDB, errorCDB, successCDB);
+    db.transaction(createDB, errorCDB, successCDB);
 }
 
 	function createDB (tx){
-		//tx.executeSql("CREATE TABLE IF NOT EXISTS datos(id_datos INT PRIMARY KEY, datos_nombre VARCHAR(50) NOT NULL);");
+		tx.executeSql("CREATE TABLE IF NOT EXISTS datos(id_datos INT PRIMARY KEY, datos_nombre VARCHAR(50) NOT NULL);");
 
 	}
 	function errorCDB (err){
@@ -64,14 +64,14 @@ function insertar(){
 }
 
 	function insertDB(tx){
-
+		tx.executeSql("insert INTO datos values ("+document.getElementById('documento').value+",'"+document.getElementById('nombre').value+"');");
 	}
 	function errInsert(err){
 		alert("Ha ocurrido un error en "+err+" al insertar los registros");
 	}
 	function succInsert(){
-		//var codigo ="<h3>Registro Guardado con éxito </h3>";
-		//$('#resultados').html(codigo);
+		var codigo ="<h3>Registro Guardado con éxito </h3>";
+		$('#resultados').html(codigo);
 	}
 
 
@@ -88,7 +88,18 @@ function consultar(){
 		/*
 			tx.executeSql("query",[],FuncionExito,FuncionFracaso);
 		*/
-		//tx.executeSql("SELECT * from datos",[],function(){alert('Exito');},function(err){alert('error al consultar');});
+		tx.executeSql("SELECT * from datos",[],function(tz, info){
+			alert('Exito');
+			var codigo="";
+			for (var i = 0; i <info.rows.length; i++) {
+				codigo+=info.rows.item(i).datos_nombre;
+				codigo+="<input type='button' value='modificar' name='modificar' onclick='modificar("+info.rows.item(i).id_datos+");'></input>";
+				codigo+="<input type='button' value='eliminar' name='eliminar' onclick='eliminar("+info.rows.item(i).id_datos+");'></input>";
+				codigo+="<br><br>";
+			}
+			$('#resultados').html(codigo);
+
+		},function(err){alert('error al consultar');});
 
 	}
 	function errCons(err){
@@ -123,19 +134,21 @@ function modificar(){
 /*
 	Función para eliminar un registro de la DB
 */
+
 function eliminar(){
+	id_datos=document.getElementById('documento').value;
 	conexion();
-	db.transaction(deletReg, errDel, succDel);
+	db.transaction(
+		function(tx){
+			tx.executeSql("delete from datos where id_datos="+id_datos+";");
+		}, errDel, succDel);
 }
-
-	function deletReg(tx){
-
-	}
+	
 	function errDel(err){
 		alert("Ha ocurrido un error en "+err+" al eliminar el registro");
 	}
 	function succDel(){
-
+		alert("se ha eliminado el registro con exito");
 	}
 
 
